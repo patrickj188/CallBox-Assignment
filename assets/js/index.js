3bd52270c3bb05722f29b6dbbf72885d253272da
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-06-24 14:30:49 -0500

    Updadte HTML and Styles for CallBox Assignment

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// document.querySelector('.dropbtn').addEventListener('mouseenter', function(){
+//     document.querySelector('.dropdown-content').style.visibility = 'visible'
+//   })
+  
+//   document.querySelector('.dropbtn').addEventListener('mouseleave', function(){
+//     document.querySelector('.dropdown-content').style.visibility = 'hidden'
+//   })
